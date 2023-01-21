08778cc36805bd33af7f6f1d792a4849a3a3d8cc
Gary Zhou <117497221+GaryZhous@users.noreply.github.com>
2023-01-20 20:57:26 -0500

    Update sort.c

--- sort.c
+++ sort.c
@@ -33,3 +33,18 @@ void bubbleSort(int*arr, int n)
                 swap(&arr[j], &arr[j + 1]);}
     }
   }
+//insertion sort
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
